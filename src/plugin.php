b7bd2288c970e43e4833c55c88295b6704b72fa4
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-01-22 10:59:29 +0530

    added missing $
    added missing $

--- src/plugin.php
+++ src/plugin.php
@@ -15,7 +15,7 @@ class Plugin
     {  
 
         $this->projectPath = dirname(__FILE__, 5);
-        echo "Project Path: ".this->projectPath."\n";
+        echo "Project Path: ".$this->projectPath."\n";
         $this->exampleFolder = "ExampleBundle";
         $bundleName = readline("Enter Bundle Name (Give name is CamelCase do not include Bundle word. Example, ExampleCode or SampleCode etc.): ");
         if (strpos($bundleName, 'Bundle') == false) {
